f13782b3b9d0205d37fb766bf5c060b7929be879
alexehouston <alexehouston@gmail.com>
2022-12-29 15:11:50 -0600

    add instagram

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,6 +9,7 @@ export default function Nav({ currentPage, setCurrentPage }) {
                 <li className="hover"><a href="https://danielkim.as.me/" target="_blank" rel="noreferrer">Booking</a></li>
                 <li className="hover" onClick={() => setCurrentPage('dates')}>Travel Dates</li>
                 <li className="hover" onClick={() => setCurrentPage('about')}>About</li>
+                <li className="hover"><a href="https://instagram.com/danielsunghoonkim/" target="_blank" rel="noreferrer"><i class="fa-brands fa-instagram"></i></a></li>
                 <li className="hover"><a href="mailto:daniel@danielsunghoonkim.com"><span className="material-symbols-outlined">mail</span></a></li>
             </ul>
         </div>
